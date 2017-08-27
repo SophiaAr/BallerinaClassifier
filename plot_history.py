67ec14b7532597b25ee769762d6abb77155eb3cb
Sophia <sophia.arakelyan@gmail.com>
2017-08-27 13:11:46 -0400

    update

---
+++ plot_history.py
@@ -0,0 +1,27 @@
+from matplotlib import pyplot as plt
+
+def plot(history):
+
+    try:
+        # summarize history for accuracy
+        plt.plot(history.history['acc'])
+        plt.plot(history.history['val_acc'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+    except:
+        print('There is no accuracy log in history variable')
+    
+    try:
+        # summarize history for loss
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+    except:
+        print('There is no loss log in history variable')
